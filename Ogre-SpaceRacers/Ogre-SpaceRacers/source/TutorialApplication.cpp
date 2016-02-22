7032dcec2e7d6dad51f01897bb7e2cb2a9cfba23
dikmanb001 <bart.dikmans@hva.nl>
2016-02-22 19:02:25 +0100

    Added a floor whose material does not feel like working and multiple light sources which creates shadows

--- Ogre-SpaceRacers/Ogre-SpaceRacers/source/TutorialApplication.cpp
+++ Ogre-SpaceRacers/Ogre-SpaceRacers/source/TutorialApplication.cpp
@@ -29,37 +29,68 @@ TutorialApplication::~TutorialApplication(void)
 //---------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
-	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
-	mCamera->setPosition(0, 47, 222);
+	//mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+	mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
 
-	Ogre::Entity* ogreEntity = mSceneMgr->createEntity("Cube.mesh");
+	//Creates the ship
+	Ogre::Entity* shipEntity = mSceneMgr->createEntity("Cube.mesh");
+	shipEntity->setCastShadows(true);
+	//shipEntity->setMaterialName("ship.material");
 	Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-	//ogreNode->setScale(2, 4, 1);
-	ogreNode->attachObject(ogreEntity);
-	/*
-	Ogre::Entity* ogreEntity2 = mSceneMgr->createEntity("robot.mesh");
-	Ogre::SceneNode* ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(
-		Ogre::Vector3(84, 48, 0));
-	ogreNode2->attachObject(ogreEntity2);
+	ogreNode->setPosition(0, 20, 20);
+	ogreNode->setScale(20, 10, 10);
+	ogreNode->attachObject(shipEntity);
+	
+	//creates a floor
+	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+	Ogre::MeshManager::getSingleton().createPlane(
+		"ground",
+		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+		plane,
+		1500, 1500, 20, 20,
+		true,
+		1, 5, 5,
+		Ogre::Vector3::UNIT_Z);
+	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+	groundEntity->setCastShadows(false);
+	groundEntity->setMaterialName("/rockwall_NH.tga");
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+	
+	//creates the blue light
+	Ogre::Light* spotLight = mSceneMgr->createLight("SpotLight");
+	spotLight->setDiffuseColour(0, 0, 1.0);
+	spotLight->setSpecularColour(0, 0, 1.0);
+	spotLight->setType(Ogre::Light::LT_SPOTLIGHT);
+	spotLight->setDirection(-1, -1, 0);
+	spotLight->setPosition(Ogre::Vector3(200, 200, 0));
+	spotLight->setSpotlightRange(Ogre::Degree(35), Ogre::Degree(50));
 
-	Ogre::Entity* ogreEntity3 = mSceneMgr->createEntity("Barrel.mesh");
-	Ogre::SceneNode* ogreNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-	ogreNode3->setPosition(0, 104, 0);
-	ogreNode3->setScale(2, 1.2, 1);
-	ogreNode3->attachObject(ogreEntity3);
-
-	Ogre::Entity* ogreEntity4 = mSceneMgr->createEntity("ninja.mesh");
-	Ogre::SceneNode* ogreNode4 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-	ogreNode4->setPosition(-84, 48, 0);
-	ogreNode4->roll(Ogre::Degree(-90));
-	ogreNode4->attachObject(ogreEntity4);
-	*/
-
-	Ogre::Light* light = mSceneMgr->createLight("MainLight");
-	light->setPosition(20, 80, 50);
+	//creates the background light
+	Ogre::Light* directionalLight = mSceneMgr->createLight("DirectionalLight");
+	directionalLight->setType(Ogre::Light::LT_DIRECTIONAL);
+	directionalLight->setDiffuseColour(Ogre::ColourValue(.4, 0, 0));
+	directionalLight->setSpecularColour(Ogre::ColourValue(.4, 0, 0));
+	directionalLight->setDirection(Ogre::Vector3(0, -1, 1));
     // Create your scene here :)
 }
 //---------------------------------------------------------------------------
+void TutorialApplication::createCamera()
+{
+	mCamera = mSceneMgr->createCamera("PlayerCam");
+	mCamera->setPosition(Ogre::Vector3(0, 300, 500));
+	mCamera->lookAt(Ogre::Vector3(0, 0, 0));
+	mCamera->setNearClipDistance(5);
+	mCameraMan = new OgreBites::SdkCameraMan(mCamera);	
+}
+
+void TutorialApplication::createViewports()
+{
+	Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+	vp->setBackgroundColour(Ogre::ColourValue(0, 0, 0));
+	mCamera->setAspectRatio(
+		Ogre::Real(vp->getActualWidth()) /
+		Ogre::Real(vp->getActualHeight()));
+}
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
