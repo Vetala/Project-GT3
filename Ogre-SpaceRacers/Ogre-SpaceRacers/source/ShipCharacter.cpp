c232e28c1ff97892410745f563ad73e384a917cd
dikmanb001 <bart.dikmans@hva.nl>
2016-03-10 11:11:07 +0100

    Added respawn

--- Ogre-SpaceRacers/Ogre-SpaceRacers/source/ShipCharacter.cpp
+++ Ogre-SpaceRacers/Ogre-SpaceRacers/source/ShipCharacter.cpp
@@ -4,25 +4,32 @@ ShipCharacter::ShipCharacter(Ogre::String name, Ogre::SceneManager *sceneMgr, in
 	// Setup basic member references
 	mName = name;
 	mSceneMgr = sceneMgr;
-	mShipHealth = shipHealth
+	mShipHealth = shipHealth;
+
+	respawning = false;
+	baseRespawnTime = 60;
+	sightNodeOffSet = Ogre::Vector3(0, 0, 100);
+	cameraNodeOffSet = Ogre::Vector3(0, 50, -100);
+	respawnNodeOffSet = Ogre::Vector3(0, 0, -100);
 
 	// Setup basic node structure to handle 3rd person cameras
 	mMainNode = mSceneMgr->getRootSceneNode()->createChildSceneNode(mName);
-	mSightNode = mMainNode->createChildSceneNode(mName + "_sight", Ogre::Vector3(0, 0, 100));
-	mCameraNode = mMainNode->createChildSceneNode(mName + "_camera", Ogre::Vector3(0, 50, -100));
+	mSightNode = mMainNode->createChildSceneNode(mName + "_sight", sightNodeOffSet);
+	mCameraNode = mMainNode->createChildSceneNode(mName + "_camera", cameraNodeOffSet);
+	mRespawnNode = mMainNode->createChildSceneNode(mName + "_respawn", respawnNodeOffSet);
 
 	// Give this character a shape 
 	mEntity = mSceneMgr->createEntity(mName, "Ship2.mesh");
 	mMainNode->attachObject(mEntity);
+	respawnTimer = baseRespawnTime;
 }
 
 void ShipCharacter::setVisible(bool visible) {
 	mMainNode->setVisible(visible);
 }
 
-int setHealth(int shipHealth, int damage){
-	shipHealth = shipHealth - damage;
-	return shipHealth;
+void ShipCharacter::doDamage(int damage){
+	mShipHealth = mShipHealth - damage;
 }
 
 ShipCharacter::~ShipCharacter() {
@@ -34,28 +41,47 @@ ShipCharacter::~ShipCharacter() {
 
 void ShipCharacter::update(Ogre::Real elapsedTime, OIS::Keyboard * input)
 {
-	if (mShipHealth < 1) {
-		//insert respawn function here
+	if (!respawning)
+	{
+		if (mShipHealth < 1 || input->isKeyDown(OIS::KC_O)) {
+			//insert respawn function here
+			mMainNode->setPosition(mRespawnNode->getPosition());
+			mSightNode->setPosition((mRespawnNode->getPosition()+sightNodeOffSet));
+			mCameraNode->setPosition((mRespawnNode->getPosition()+cameraNodeOffSet));
+			mRespawnNode->setPosition((mRespawnNode->getPosition()+respawnNodeOffSet));
+			respawning = true;
+		}
+		if (input->isKeyDown(OIS::KC_W)) {
+			mMainNode->translate(mMainNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
+			mSightNode->translate(mSightNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
+			mCameraNode->translate(mCameraNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
+			mRespawnNode->translate(mRespawnNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
+		}
+		if (input->isKeyDown(OIS::KC_S)) {
+			mMainNode->translate(mMainNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
+			mSightNode->translate(mSightNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
+			mCameraNode->translate(mCameraNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
+			mRespawnNode->translate(mRespawnNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
+		}
+		if (input->isKeyDown(OIS::KC_A)) {
+			mMainNode->yaw(Ogre::Radian(2 * elapsedTime));
+			mSightNode->yaw(Ogre::Radian(2 * elapsedTime));
+			mCameraNode->yaw(Ogre::Radian(2 * elapsedTime));
+			mRespawnNode->yaw(Ogre::Radian(2 * elapsedTime));
+		}
+		if (input->isKeyDown(OIS::KC_D)) {
+			mMainNode->yaw(Ogre::Radian(-2 * elapsedTime));
+			mSightNode->yaw(Ogre::Radian(-2 * elapsedTime));
+			mCameraNode->yaw(Ogre::Radian(-2 * elapsedTime));
+			mRespawnNode->yaw(Ogre::Radian(-2 * elapsedTime));
+		}
 	}
-	if (input->isKeyDown(OIS::KC_W)) {
-		mMainNode->translate(mMainNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
-		mSightNode->translate(mSightNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
-		mCameraNode->translate(mCameraNode->getOrientation() * Ogre::Vector3(0, 0, 100 * elapsedTime));
-	}
-	if (input->isKeyDown(OIS::KC_S)) {
-		mMainNode->translate(mMainNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
-		mSightNode->translate(mSightNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
-		mCameraNode->translate(mCameraNode->getOrientation() * Ogre::Vector3(0, 0, -50 * elapsedTime));
-	}
-	if (input->isKeyDown(OIS::KC_A)) {
-		mMainNode->yaw(Ogre::Radian(2 * elapsedTime));
-		mSightNode->yaw(Ogre::Radian(2 * elapsedTime));
-		mCameraNode->yaw(Ogre::Radian(2 * elapsedTime));
-	}
-	if (input->isKeyDown(OIS::KC_D)) {
-		mMainNode->yaw(Ogre::Radian(-2 * elapsedTime));
-		mSightNode->yaw(Ogre::Radian(-2 * elapsedTime));
-		mCameraNode->yaw(Ogre::Radian(-2 * elapsedTime));
+	else {
+		respawnTimer--;
+		if(respawnTimer < 0){
+			respawning = false;
+			respawnTimer = baseRespawnTime;
+		}
 	}
 }
 
--- Ogre-SpaceRacers/Ogre-SpaceRacers/source/ShipCharacter.h
+++ Ogre-SpaceRacers/Ogre-SpaceRacers/source/ShipCharacter.h
@@ -9,11 +9,20 @@ protected:
 
 public:
 	Ogre::SceneNode *mMainNode;
+	Ogre::SceneNode *mRespawnNode;
+	Ogre::Vector3 cameraNodeOffSet;
+	Ogre::Vector3 sightNodeOffSet;
+	Ogre::Vector3 respawnNodeOffSet;
+
 	ShipCharacter(Ogre::String name, Ogre::SceneManager *sceneMgr, int shipHealth);
 	~ShipCharacter();
 	void update(Ogre::Real elapsedTime, OIS::Keyboard * input);
 	void setVisible(bool visible);
-	int setHealth(int shipHealth, int damage);//if a health pickup is picked up just call upon this function and make the damage negative. There is currently no max health implemented
+
+	void doDamage(int damage);//if a health pickup is picked up just call upon this function and make the damage negative. There is currently no max health implemented
+	bool respawning;
+	int respawnTimer;
+	int baseRespawnTime;
 };
 
 
