456b0490f1a330244bbcd650bfa16a78d7612e4a
Jan-Wllem Jozic <Jan-willem.jozic@hva.nl>
2016-03-14 13:17:42 +0100

    Split world up
    Split up the world

--- Ogre-SpaceRacers/Ogre-SpaceRacers/source/World_1.cpp
+++ Ogre-SpaceRacers/Ogre-SpaceRacers/source/World_1.cpp
@@ -5,6 +5,9 @@
 World_1::World_1(Ogre::SceneManager *mSceneMgr)
 {
 	//creates rocks
-	World1 = new Object_WorldObject("World_1", mSceneMgr, "World_1_part1", (Ogre::Vector3(0, 0, 0)), (Ogre::Vector3(20, 20, 20)));
-	World2 = new Object_WorldObject("World_2", mSceneMgr, "World_1_part2", (Ogre::Vector3(750, 0, -100)), (Ogre::Vector3(30, 20, 40)));
+	World1 = new Object_WorldObject("World_1", mSceneMgr, "World_1_part1", (Ogre::Vector3(0, -6, 150)), (Ogre::Vector3(3, 3, 3)));
+	World2 = new Object_WorldObject("World_2", mSceneMgr, "World_1_part2", (Ogre::Vector3(0, -6, 150)), (Ogre::Vector3(3, 3, 3)));
+	World3 = new Object_WorldObject("World_3", mSceneMgr, "World_1_part3", (Ogre::Vector3(0, -6, 150)), (Ogre::Vector3(3, 3, 3)));
+	World4 = new Object_WorldObject("World_4", mSceneMgr, "World_1_part4", (Ogre::Vector3(0, -6, 150)), (Ogre::Vector3(3, 3, 3)));
+	World5 = new Object_WorldObject("World_5", mSceneMgr, "World_1_part5", (Ogre::Vector3(0, -6, 150)), (Ogre::Vector3(3, 3, 3)));
 }
